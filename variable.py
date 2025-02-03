d86e532aaacb6adb77fc50a6cb9f7353f52252b6
shamzszz <hasham.ian2006@gmail.com>
2025-02-03 10:23:38 +0000

    initial commit

---
+++ variable.py
@@ -0,0 +1,17 @@
+#Variable is a container used to store a value
+number = 10
+firstname = "Hasham"
+lastname = "Ian"
+language = "python"
+a = 10
+b = 15
+
+
+
+print(number)
+print(firstname)
+print(lastname)
+print(firstname,lastname)
+print(language,"is a web programming language")
+print(a + b)
+print("The sum of",a, "and",b, "is",a + b)
