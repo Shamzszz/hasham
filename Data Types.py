0abe11ad260dbe00370584ab813520730ac2a6e8
shamzszz <hasham.ian2006@gmail.com>
2025-02-04 10:17:30 +0000

    second commit

---
+++ Data Types.py
@@ -0,0 +1,28 @@
+number = 45 #int
+num = 66.78 #float
+text = "hello" #str
+ismammal = True #boolean
+
+#Data Structures
+cars = {"Toyota", "Mercedes","Ford"} #List
+fruits = ("banana","apple","grape") #tuple
+countries = {"England","Italy","Germany"} #set
+details ={
+    "firstname": "brian",
+    "course" : "MIT",
+    "age" : 19,
+    "maritalstatus" : "single"}
+
+print(number)
+print(num)
+print(text)
+print(ismammal)
+print(cars)
+print(fruits)
+print(countries)
+print(details)
+print(details["course"])
+
+#typecasting
+print(float(number))
+print(int(num))
